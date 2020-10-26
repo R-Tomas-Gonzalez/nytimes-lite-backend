6e5efe2d3858e60b3b49d6eb3e9f39529e2eb188
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-10-26 16:27:15 -0500

    added files to deploy on heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_nytimes_lite", domain: "nytimeslite.com"
+    Rails.application.config.session_store :cookie_store, key: "_nytimes_lite", domain: "nytimes-lite.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_nytimes_lite"
 end
